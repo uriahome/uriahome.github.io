ca161bda50bfe811ea2697a7a690f3295e4a1dca
uriatyan <ryouma_1106@yahoo.co.jp>
2017-07-20 19:10:28 +0900

    ガチャ絵を試しに6枚の画像で実装してみた（1週間前に 今夜はミニ餃子だったので初コミットです 明日はイカの発売日ですね

--- gacha.js
+++ gacha.js
@@ -1,4 +1,12 @@
 (function(){
 //画像をランダムに表示する感じにしたい
 //フォルダの中身を適当に拾うようにすると楽だったけどフォルダをいじるのは難しそうなのでもう最初に何枚か決めて宣言してしまうかー種類が多くて面倒臭いのだよなー
+$('#gachabutton').click(function(){
+     var gacha = Math.floor(Math.random()*6);
+    //document.write("<img src='illust/gacha/"+gacha+".PNG' width='128' height='128'>");
+    $('#gacha-area').html("<img src='illust/gacha/"+gacha+".PNG' width='128' height='128'>");
+    
+});
+   // var gacha = Math.floor(Math.random()*6);
+    //document.write("<img src='illust/gacha/"+gacha+".PNG' width='128' height='128'>");
 })();
